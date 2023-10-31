34834e4760c7aa54831e8e382ddf63a8ce7ec02a
Immanuel Shin <Immanuel.shin18@gmail.com>
2023-10-31 19:00:23 +0000

    Added minimum staff function

--- js/app.js
+++ js/app.js
@@ -18,20 +18,20 @@ function runTables() {
 
 // Create base table
 function createTable() {
-  let storeSales = getID('storeSales');
-  let section = cEl('section');
+  const storeSales = getID('storeSales');
+  const section = cEl('section');
   section.setAttribute('id', 'tableSection');
   storeSales.append(section);
 
-  let table = cEl('table');
+  const table = cEl('table');
   table.setAttribute('id', 'saleTable');
   section.append(table);
 
-  let tRH = cEl('tr');
+  const tRH = cEl('tr');
   tRH.setAttribute('id', 'firstRow');
   table.appendChild(tRH);
 
-  let tHLocation = cEl('th');
+  const tHLocation = cEl('th');
   tHLocation.textContent = 'Locations';
   tRH.appendChild(tHLocation);
 
@@ -45,16 +45,16 @@ function createTable() {
     tRH.appendChild(tH);
   }
 
-  let tHTotal = cEl('th');
+  const tHTotal = cEl('th');
   tHTotal.textContent = 'Location Totals';
   tRH.appendChild(tHTotal);
 
   // Table row header end
-  let tRHE = cEl('tr');
+  const tRHE = cEl('tr');
   tRHE.setAttribute('id', 'tRHE');
   table.appendChild(tRHE);
 
-  let tHLocationEnd = cEl('th');
+  const tHLocationEnd = cEl('th');
   tHLocationEnd.textContent = 'Hourly Totals for All Locations';
   tRHE.appendChild(tHLocationEnd);
 }
@@ -76,19 +76,15 @@ function renderStores() {
 
 // Adds table columns together to get total daily values.
 function getTableTotals() {
-  let table = getID('saleTable');
-  let row = getID('tRHE');
-  console.log('test');
+  const table = getID('saleTable');
+  const row = getID('tRHE');
   for (let x = 1; x < table.rows[0].cells.length; x++) {
-    console.log(table.rows[0].cells.length);
-    let tH = cEl('th');
+    const tH = cEl('th');
     let total = 0;
     for (let i = 1; i < (table.rows.length - 1); i++){
       let number = table.rows[i].cells[x].childNodes.item(0).data;
-      console.log((number));
       total += Number(number);
     }
-    console.log(total);
     tH.textContent = total;
     row.appendChild(tH);
   }
@@ -103,67 +99,84 @@ function Store(name, hours, contact, address, min, max, average) {
   this.minCus = min;
   this.maxCus = max;
   this.cookiePer = average;
-  let cookies = [];
-  for (let x = 6; x <= 19; x++) {
-    cookies[x-6] = getCookies(x, this.maxCus, this.cookiePer);
-  }
-  this.cookies = cookies;
+  this.cookies = getCookies(this.maxCus, this.cookiePer);
+  this.staff = getStaff(this.maxCus);
 }
 
 // Method to append store sales onto table.
 Store.prototype.render = function() {
-  let table = getID('saleTable');
+  const table = getID('saleTable');
 
-  let lastRowIndex = table.rows.length;
-  let row = table.insertRow(lastRowIndex - 1);
+  const lastRowIndex = table.rows.length;
+  const row = table.insertRow(lastRowIndex - 1);
   row.setAttribute('id', this.name);
 
-  let tdName = cEl('td');
+  const tdName = cEl('td');
   tdName.textContent = this.name;
   row.appendChild(tdName);
 
   for (let x of this.cookies) {
-    let tdCookie = cEl('td');
+    const tdCookie = cEl('td');
     tdCookie.textContent = x;
     row.appendChild(tdCookie);
   }
-  let total = cEl('th');
+  const total = cEl('th');
   total.textContent = this.cookies.reduce((acc, c) => acc + c, 0);
   row.appendChild(total);
 };
 
 // Method to print store information onto index.html page
 Store.prototype.inform = function() {
-  let storeSection = getID('storeInfo');
+  const storeSection = getID('storeInfo');
 
-  let section = cEl('section');
+  const section = cEl('section');
   section.setAttribute('class', 'storeInformation');
   storeSection.append(section);
 
-  let h3 = cEl('h3');
+  const h3 = cEl('h3');
   h3.textContent = this.name;
   section.append(h3);
 
-  let ul = cEl('ul');
+  const ul = cEl('ul');
   section.append(ul);
 
-  let liHour = cEl('li');
+  const liHour = cEl('li');
   liHour.textContent = 'Hours Open: ' + this.hours;
   ul.append(liHour);
 
-  let liContact = cEl('li');
+  const liContact = cEl('li');
   liContact.textContent = 'Contact Info: ' + this.contact;
   ul.append(liContact);
 
-  let liLocation = cEl('li');
+  const liLocation = cEl('li');
   liLocation.textContent = 'Location: ' + this.address;
   ul.append(liLocation);
 };
 
 // Get # of cookies. Generates a random number of customers and multiplies them by average cookies per customer.
-function getCookies(hour, max, average) {
+function getCookies(max, average) {
   const traffic = [0.5, 0.75, 1.0, 0.6, 0.8, 1.0, 0.7, 0.4, 0.6, 0.9, 0.7, 0.5, 0.3, 0.4, 0.6];
-  return Math.ceil(max * traffic[hour-6] * average);
+  const cookies = [];
+  for (let x = 0; x <= 13; x++) {
+    cookies[x] = Math.ceil(max * traffic[x] * average);
+  }
+  return cookies;
+}
+
+function getStaff(max) {
+  const traffic = [0.5, 0.75, 1.0, 0.6, 0.8, 1.0, 0.7, 0.4, 0.6, 0.9, 0.7, 0.5, 0.3, 0.4, 0.6];
+  const staff = [];
+  let i = 0;
+  for (let x of traffic) {
+    let reqStaff = Math.ceil(x * max / 20);
+    if (reqStaff <= 2) {
+      staff[i] = 2;
+    } else {
+      staff[i] = reqStaff;
+    }
+    i++;
+  }
+  return staff;
 }
 
 // Repetitive get ID function
