672f479dcfc092c275535c3e422a99d08076937d
Immanuel Shin <Immanuel.shin18@gmail.com>
2023-10-31 18:23:40 +0000

    Moved print/append functions into prototype methods

--- js/app.js
+++ js/app.js
@@ -7,11 +7,12 @@ const dubai = new Store('Dubai', '6am - 7pm', '333-333-3333', '1 Sheikh Mohammed
 const paris = new Store('Paris', '6am - 7pm', '444-444-4444', 'Champ de Mars, 5 Avenue Anatole France, 75007 Paris', 20, 38, 2.3);
 const lima = new Store('Lima', '6am - 7pm', '555-555-5555', 'Ca. Gral. Borgoño cuadra 8, Miraflores 15074)', 2, 16, 4.6);
 
+const storeArray = [seattle, tokyo, dubai, paris, lima];
 
 // Run tables
 function runTables() {
   createTable();
-  loadStoreSales();
+  renderStores();
   getTableTotals();
 }
 
@@ -60,21 +61,17 @@ function createTable() {
 
 
 // Function to print all store info. Called on index.html load
-function loadStoreInfo() {
-  printStoreInfo(seattle);
-  printStoreInfo(tokyo);
-  printStoreInfo(dubai);
-  printStoreInfo(paris);
-  printStoreInfo(lima);
+function informStores() {
+  for (let x of storeArray) {
+    x.inform();
+  }
 }
 
-// Function to append all store sales info to table.
-function loadStoreSales() {
-  printStoreSales(seattle);
-  printStoreSales(tokyo);
-  printStoreSales(dubai);
-  printStoreSales(paris);
-  printStoreSales(lima);
+// Calls render method for all stores. Essentially prints store info to table.
+function renderStores() {
+  for (let x of storeArray) {
+    x.render();
+  }
 }
 
 // Adds table columns together to get total daily values.
@@ -97,56 +94,6 @@ function getTableTotals() {
   }
 }
 
-// Prints store information
-function printStoreInfo(store) {
-  let storeSection = getID('storeInfo');
-
-  let section = cEl('section');
-  section.setAttribute('class', 'storeInformation');
-  storeSection.append(section);
-
-  let h3 = cEl('h3');
-  h3.textContent = store.name;
-  section.append(h3);
-
-  let ul = cEl('ul');
-  section.append(ul);
-
-  let liHour = cEl('li');
-  liHour.textContent = 'Hours Open: ' + store.hours;
-  ul.append(liHour);
-
-  let liContact = cEl('li');
-  liContact.textContent = 'Contact Info: ' + store.contact;
-  ul.append(liContact);
-
-  let liLocation = cEl('li');
-  liLocation.textContent = 'Location: ' + store.address;
-  ul.append(liLocation);
-}
-
-// Prints store sales info
-function printStoreSales(store) {
-  let table = getID('saleTable');
-
-  let lastRowIndex = table.rows.length;
-  let row = table.insertRow(lastRowIndex - 1);
-  row.setAttribute('id', store.name);
-
-  let tdName = cEl('td');
-  tdName.textContent = store.name;
-  row.appendChild(tdName);
-
-  for (let x of store.cookies) {
-    let tdCookie = cEl('td');
-    tdCookie.textContent = x;
-    row.appendChild(tdCookie);
-  }
-  let total = cEl('th');
-  total.textContent = store.cookies.reduce((acc, c) => acc + c, 0);
-  row.appendChild(total);
-}
-
 // Store object constructor
 function Store(name, hours, contact, address, min, max, average) {
   this.name = name;
@@ -158,23 +105,65 @@ function Store(name, hours, contact, address, min, max, average) {
   this.cookiePer = average;
   let cookies = [];
   for (let x = 6; x <= 19; x++) {
-    cookies[x-6] = getCookies(x, this.minCus, this.maxCus, this.cookiePer);
+    cookies[x-6] = getCookies(x, this.maxCus, this.cookiePer);
   }
   this.cookies = cookies;
 }
 
-// Get # of cookies. Generates a random number of customers and multiplies them by average cookies per customer.
-function getCookies(hour, min, max, average) {
-  if (hour < 10) {
-    let minMax = Math.ceil(min * (Math.random() + 1));
-    return Math.ceil(Math.ceil(Math.random() * (minMax - min) + min) * average);
-  } else if (hour < 15) {
-    let maxMin = Math.ceil(max * ((Math.random() * 0.25) + 0.75));
-    return Math.ceil(Math.ceil(Math.random() * (max - maxMin) + maxMin) * average);
-  } else {
-    let maxMinEnd = Math.ceil(max * ((Math.random() * 0.33) + 0.33));
-    return Math.ceil(Math.ceil(Math.random() * (max - maxMinEnd) + min) * average);
+// Method to append store sales onto table.
+Store.prototype.render = function() {
+  let table = getID('saleTable');
+
+  let lastRowIndex = table.rows.length;
+  let row = table.insertRow(lastRowIndex - 1);
+  row.setAttribute('id', this.name);
+
+  let tdName = cEl('td');
+  tdName.textContent = this.name;
+  row.appendChild(tdName);
+
+  for (let x of this.cookies) {
+    let tdCookie = cEl('td');
+    tdCookie.textContent = x;
+    row.appendChild(tdCookie);
   }
+  let total = cEl('th');
+  total.textContent = this.cookies.reduce((acc, c) => acc + c, 0);
+  row.appendChild(total);
+};
+
+// Method to print store information onto index.html page
+Store.prototype.inform = function() {
+  let storeSection = getID('storeInfo');
+
+  let section = cEl('section');
+  section.setAttribute('class', 'storeInformation');
+  storeSection.append(section);
+
+  let h3 = cEl('h3');
+  h3.textContent = this.name;
+  section.append(h3);
+
+  let ul = cEl('ul');
+  section.append(ul);
+
+  let liHour = cEl('li');
+  liHour.textContent = 'Hours Open: ' + this.hours;
+  ul.append(liHour);
+
+  let liContact = cEl('li');
+  liContact.textContent = 'Contact Info: ' + this.contact;
+  ul.append(liContact);
+
+  let liLocation = cEl('li');
+  liLocation.textContent = 'Location: ' + this.address;
+  ul.append(liLocation);
+};
+
+// Get # of cookies. Generates a random number of customers and multiplies them by average cookies per customer.
+function getCookies(hour, max, average) {
+  const traffic = [0.5, 0.75, 1.0, 0.6, 0.8, 1.0, 0.7, 0.4, 0.6, 0.9, 0.7, 0.5, 0.3, 0.4, 0.6];
+  return Math.ceil(max * traffic[hour-6] * average);
 }
 
 // Repetitive get ID function
